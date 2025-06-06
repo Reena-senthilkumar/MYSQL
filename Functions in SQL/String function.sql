95e36ba5bd0b82628d04a2d704f3af30a346f402
Reena S <160510029+Reena-senthilkumar@users.noreply.github.com>
2025-06-06 19:39:06 +0530

    Create String function.sql

---
+++ Functions in SQL/String function.sql
@@ -0,0 +1,8 @@
+#UPPER()
+       SELECT UPPER (name) from student;
+
+#LOWER()
+       SELECT LOWER(name) FROM student;
+
+##CONCAT()
+      SELECT CONCAT(firstname,lastname) AS FULLNAME from student;
