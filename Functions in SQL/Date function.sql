2477fb8bf8dcad42ec2059d600abcc4ed9d3d76c
Reena S <160510029+Reena-senthilkumar@users.noreply.github.com>
2025-06-07 21:38:39 +0530

    Create Date function.sql

---
+++ Functions in SQL/Date function.sql
@@ -0,0 +1,15 @@
+#Now():To print date and time 
+  cmd: SELECT NOW();
+
+  output:2025-05-31 15:23
+
+
+#Date():To print date only 
+   cmd:SELECT DATE();
+
+    output:2025-05-31
+
+#DATEDIFF():To print different between two dates
+   cmd: SELECT DATEDIFF('2025-06-10','25-5-31);
+   
+    output:10
