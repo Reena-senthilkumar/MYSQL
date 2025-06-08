d8efd4be7c507e0930a50424bf0ce9175e98a33a
Reena S <160510029+Reena-senthilkumar@users.noreply.github.com>
2025-06-08 20:32:13 +0530

    Update String function.sql

--- Functions in SQL/String function.sql
+++ Functions in SQL/String function.sql
@@ -1,8 +1,8 @@
-#UPPER()
+#UPPER():To print all the data in name column in capital letter
        SELECT UPPER (name) from student;
 
-#LOWER()
+#LOWER():To print all the data in name column in small letter
        SELECT LOWER(name) FROM student;
 
-##CONCAT()
+##CONCAT():To print all the data in firstname and lastname in same column which name is  FULLNAME
       SELECT CONCAT(firstname,lastname) AS FULLNAME from student;
