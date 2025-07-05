7b4f5de47d367ba70622790706524e08adcb44c9
Reena S <160510029+Reena-senthilkumar@users.noreply.github.com>
2025-07-05 12:40:36 +0530

    Update Date function.sql

--- Functions in SQL/Date function.sql
+++ Functions in SQL/Date function.sql
@@ -1,15 +1,30 @@
-#Now():To print date and time 
+--#Now():To print date and time 
   cmd: SELECT NOW();
 
   output:2025-05-31 15:23
 
 
-#Date():To print date only 
+--#Date():To print date only 
    cmd:SELECT DATE();
 
     output:2025-05-31
+      
+    --  #month():To print date only 
+   cmd:SELECT MONTH();
 
-#DATEDIFF():To print different between two dates
+    output:05
+
+   --   #year():To print date only 
+   cmd:SELECT YEAR();
+
+    output:2025
+
+     -- #Time():To print date only 
+   cmd:SELECT TIME();
+
+    output:15:23
+
+--#DATEDIFF():To print different between two dates
    cmd: SELECT DATEDIFF('2025-06-10','25-5-31);
    
     output:10
