1c0f3df0a72d52e73db7cc5fc72aa01bdad8e8a5
Reena S <160510029+Reena-senthilkumar@users.noreply.github.com>
2025-08-17 09:18:31 +0530

    Create IN.sql

---
+++ Basic Queries/IN.sql
@@ -0,0 +1,8 @@
+mysql> Select * from student where name in("barath","venkat");
++--------+-------+------+
+| name   | marks | dept |
++--------+-------+------+
+| barath |    67 | CSE  |
+| venkat |    89 | ECE  |
++--------+-------+------+
+2 rows in set (0.00 sec)
